ddb32710910205e0683ab8be31a696f9de9f3a1e
Vuk Mirovic <wooque@gmail.com>
2016-12-29 10:45:44 +0100

    Add .gitignore, reformat code

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {"action": "encrypt"});
     });
 });
--- content.js
+++ content.js
@@ -4,23 +4,22 @@ var header = "MailSec encrypted ";
 function ua2hex(ua) {
     var hex = "";
     for (var i = 0; i < ua.length; i++) {
-        hex += (ua[i] < 16 ? "0": "") + ua[i].toString(16);
+        hex += (ua[i] < 16 ? "0" : "") + ua[i].toString(16);
     }
     return hex;
 }
 
 function hex2ua(hex) {
-    var ua = new Uint8Array(hex.length/2);
+    var ua = new Uint8Array(hex.length / 2);
     for (var i = 0; i < hex.length; i += 2) {
-        ua[i/2] = parseInt(hex.slice(i, i+2), 16);
+        ua[i / 2] = parseInt(hex.slice(i, i + 2), 16);
     }
     return ua;
 }
 
 function get_key(password) {
     var pass_bytes = new TextEncoder().encode(password);
-    return crypto.subtle.digest({name: "SHA-256"}, pass_bytes)
-    .then(function(hash) {
+    return crypto.subtle.digest({name: "SHA-256"}, pass_bytes).then(function (hash) {
         return crypto.subtle.importKey("raw", hash, {name: "AES-CTR"}, false, ["encrypt", "decrypt"]);
     });
 }
@@ -30,15 +29,15 @@ function encrypt_txt(txt, password) {
     var encrypted_data = new Uint8Array(16 + data.length);
     var counter = crypto.getRandomValues(new Uint8Array(16));
     encrypted_data.set(counter);
-    
+
     return get_key(password).then(function (key) {
         return crypto.subtle.encrypt({name: "AES-CTR", counter: counter, length: 128}, key, data)
-        .then(function(encrypted){
-            encrypted_data.set(new Uint8Array(encrypted), 16);
-            return new Promise(function(resolve, reject) {
-                resolve(ua2hex(encrypted_data));
+            .then(function (encrypted) {
+                encrypted_data.set(new Uint8Array(encrypted), 16);
+                return new Promise(function (resolve, reject) {
+                    resolve(ua2hex(encrypted_data));
+                });
             });
-        });
     });
 }
 
@@ -49,29 +48,29 @@ function decrypt_txt(txt, password) {
 
     return get_key(password).then(function (key) {
         return crypto.subtle.decrypt({name: "AES-CTR", counter: counter, length: 128}, key, data)
-        .then(function(decrypted){
-            var orig_txt = new TextDecoder().decode(new Uint8Array(decrypted));
-            return new Promise(function(resolve, reject) {
-                resolve(orig_txt);
+            .then(function (decrypted) {
+                var orig_txt = new TextDecoder().decode(new Uint8Array(decrypted));
+                return new Promise(function (resolve, reject) {
+                    resolve(orig_txt);
+                });
             });
-        });
     });
 }
 
 function get_send_mail_elem_gmail() {
     var send = document.getElementsByClassName('Am Al editable LW-avf');
     if (send.length == 0) return;
-    
+
     return send[0];
 }
 
 function get_send_mail_elem_outlook() {
     var send = document.getElementById('divtagdefaultwrapper');
     if (send) return send;
-    
+
     send = document.getElementsByClassName('_mcp_W1');
     if (send.length == 0) return;
-    
+
     return send[0];
 }
 
@@ -82,28 +81,29 @@ function get_send_mail_elem() {
 function encrypt_email() {
     var send = get_send_mail_elem();
     if (!send) return;
-    
+
     var text = send.innerHTML;
     if (text.startsWith(header)) return;
-    
-    return encrypt_txt(text, password)
-    .then(function(enc_txt) {
+
+    return encrypt_txt(text, password).then(function (enc_txt) {
         send.innerHTML = header + enc_txt;
-        return new Promise(function (resolve, reject) { resolve(); });
+        return new Promise(function (resolve, reject) {
+            resolve();
+        });
     });
-};
+}
 
 function get_send_button_gmail() {
     var send = document.getElementsByClassName('T-I J-J5-Ji aoO T-I-atl L3');
     if (send.length == 0) return;
-    
+
     return send[0];
 }
 
 function get_send_button_outlook() {
     var send = document.getElementsByClassName('_mcp_Z1');
     if (send.length == 0) return;
-    
+
     for (var i = 0; i < send.length; i++) {
         if (send[i].innerText == "Send") {
             return send[i];
@@ -118,19 +118,19 @@ function get_send_button() {
 function attach_on_send() {
     var send = get_send_button();
     if (!send) return;
-    
+
     if (send.getAttribute("attached")) return;
     send.setAttribute("attached", true);
-    
+
     var triggered = false;
-    send.addEventListener("click", function(e) {
+    send.addEventListener("click", function (e) {
         if (triggered) {
             triggered = false;
             return;
         }
         e.stopImmediatePropagation();
-        
-        encrypt_email().then(function() {
+
+        encrypt_email().then(function () {
             triggered = true;
             send.click();
         });
@@ -140,13 +140,12 @@ function attach_on_send() {
 function get_mail_elem_gmail() {
     var msg = document.getElementsByClassName('a3s aXjCH');
     if (msg.length == 0) return;
-    
+
     return msg[0].childNodes[0];
 }
 
 function get_mail_elem_outlook() {
-    var msg = document.getElementById('x_divtagdefaultwrapper');
-    return msg;
+    return document.getElementById('x_divtagdefaultwrapper');
 }
 
 function get_mail_elem() {
@@ -156,23 +155,24 @@ function get_mail_elem() {
 function decrypt_email() {
     var msg = get_mail_elem();
     if (!msg) return;
-    
-    var text = msg.innerHTML.replace(/(\<wbr\>|\n)/g, '');
+
+    var text = msg.innerHTML.replace(/(<wbr>|\n)/g, '');
     if (!text.startsWith(header)) return;
-    
-    return decrypt_txt(text.slice(header.length), password)
-    .then(function (dec_txt) {
+
+    return decrypt_txt(text.slice(header.length), password).then(function (dec_txt) {
         msg.innerHTML = dec_txt;
-        return new Promise(function (resolve, reject) { resolve(); });
+        return new Promise(function (resolve, reject) {
+            resolve();
+        });
     });
 }
 
-chrome.runtime.onMessage.addListener(function() {
+chrome.runtime.onMessage.addListener(function () {
     encrypt_email();
     decrypt_email();
 });
 
-document.addEventListener("DOMSubtreeModified", function() {    
+document.addEventListener("DOMSubtreeModified", function () {
     attach_on_send();
     decrypt_email();
 });
